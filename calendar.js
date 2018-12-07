953f3ec8480d41f9b5a1c0184fb3587b0e0f36f5
CelXNaga <CelXNaga@yandex.ru>
2018-12-08 02:10:23 +0300

    first part complete

--- calendar.js
+++ calendar.js
@@ -1,45 +1,71 @@
-var year = 2000;
-var month = 1;
-
 var calendar = document.querySelector('#calendar');
 var dates = document.querySelector('.dates');
+console.log(dates);
+console.log(calendar);
+build(2018, 11, dates);
 
-//Функция построения календаря
-build(year, month, dates);
 
+//Главная функция, которая создает календарь
 function build(year, month, dates) {
 	var arr = [];
-	var from = 1;
-	var to = getLastDay(year, month);
-	console.log(to);
+	var firstDateOfMonth = 1;  //определяем с какого числа начинается месяц. Это константа.
+	var lastDateOfMonth = getLastDay(year, month); //определяем какой датой заканчивается месяц.
+	//определяем с какого дня недели начинается месяц. 
+	//буквально мы определяем сколько "пустых" дней в неделе будет перед первым числом
+	var preNum = getPreNum(year, month);
+	//определяем сколько "пустых" дней будет в конце месяца
+	var postNum = getPostNum(year, month);
 
-	//var preNum = getPreNum();
-	//var postNum = getPreNum();
-
-
-	//arr = createArr(from, to);
-	//arr = shiftElems(preNum, arr);
-	//arr = popElems(postNum, arr);
-	//arr = matrix(7, arr);
+	//создание масива чисел в месяце
+	arr = createArr(firstDateOfMonth, lastDateOfMonth);
 	
-	//createTable(arr, dates);
+	//добавляет в начало массива "пустые" дни недели, чтобы в итоге получилось 7*5 ячеек
+	arr = addEmptyElems(preNum, '', arr);
+	//добавляем в конец массива "пустые" дни недели, чтобы в итоге получилось 7*5 ячеек
+	arr = addToEndEmptyElems(postNum, '', arr);
+	//разбивает массив на 7 элементов, создавая двухмерный массив
+	arr = matrix(7, arr);
+	createTable(arr, dates);
 }
+//сюда передается уже двумерный массив, поэтому цикл будет двойной
+//i2 - массив, элементы которого массивы
+//i - под массив, элементы которого числа
 
 function createTable(arr, parent) {
-
+	for (var i2 = 0; i2 < arr.length; i2++) {
+		var tr = document.createElement('tr');
+		for (var i = 0; i < arr[i2].length; i++) {
+			var td = document.createElement('td');
+			td.innerHTML = arr[i2][i];
+			tr.appendChild(td);
+		}
+		parent.appendChild(tr); 
+	}
 }
 
+//создание масива чисел в месяце
 function createArr(from, to) {
-
+	var arr = [];
+	for (var i = from; i <= to; i++) {
+		arr.push(i);
+	}
+	return arr;
 }
-
-function shiftElems(num, arr) {
-
+//добавляет в начало массива "пустые" дни недели, чтобы в итоге получилось 7*5 ячеек
+function addEmptyElems(num, elem, arr) {
+	for (var i = 0; i < num; i++) {
+		arr.unshift(elem);
+	}
+	return arr;
 }
-
-function popElems(num, arr) {
-
+//добавляем в конец массива "пустые" дни недели, чтобы в итоге получилось 7*5 ячеек
+function addToEndEmptyElems(num, elem, arr) {
+	for (var i = 0; i < num; i++) {
+		arr.push(elem);
+	}
+	return arr;
 }
+//Эта функция определяет количество дней в конкретном месяце
 function getLastDay(year, month) {
 	if (month == 1) {
 		if (isLeap(year)) {
@@ -53,6 +79,7 @@ function getLastDay(year, month) {
 		return days[month];
 	}
 }
+//Эта функция определяет является ли этот конкретный год високосным
 function isLeap(year) {
 	if ((year % 4 == 0 &&  year % 100 != 0) || year % 400 == 0) { 
 		return true;
@@ -60,12 +87,43 @@ function isLeap(year) {
 		return false;
 	}
 }
-function getPreNum() {
-
+//определяем с какого дня недели начинается месяц. 
+//буквально мы определяем сколько "пустых" дней в неделе будет перед первым числом
+function getPreNum(year, month) {
+	var JSFirstDay = getFirstWeekDayOfMonthNum(year, month);
+	var RealFirstDay = getRealDayOfWeekNum(JSFirstDay);
+	return RealFirstDay - 1;
 }
-function getPostNum() {
-
+//определяем сколько "пустых" дней будет в конце месяца
+function getPostNum(year, month) {
+	var JSFirstDay = getLastWeekDayOfMonthNum(year, month);
+	var RealFirstDay = getRealDayOfWeekNum(JSFirstDay);
+	return 7 - RealFirstDay;
 }
-function matrix() {
+//разбивает массив на 7 элементов, создавая 2х мерный массив
+function matrix(num, arr) {
+	var result = [];
+	var chunk = [];
+	var iterCount = arr.length / num;
+	for (var i = 0; i < iterCount; i++) {
+		chunk = arr.splice(0, num);
+		result.push(chunk);
+	}
+	return result;
+}
 
+function getRealDayOfWeekNum(num) {
+	if (num == 0) {
+		return 7;
+	} else {
+		return num;
+	}
+}
+function getFirstWeekDayOfMonthNum(year, month) {
+	var date = new Date(year, month, 1);
+	return date.getDay();
+}
+function getLastWeekDayOfMonthNum(year, month) {
+	var date = new Date(year, month + 1, 0);
+	return date.getDay();
 }
